b4a9c9058fd3c18afa1e92fe609aad18ae65d499
SwarmOfBears <scorchyscorch@gmail.com>
2023-07-23 18:14:54 -0500

    Initial commit

---
+++ src/main/java/me/swarmofbears/worldeditbrushnames/CommandListener.java
@@ -0,0 +1,2 @@
+package me.swarmofbears.worldeditbrushnames;public class CommandListener {
+}
---
+++ src/main/java/me/swarmofbears/worldeditbrushnames/WorldeditBrushNames.java
@@ -0,0 +1,17 @@
+package me.swarmofbears.worldeditbrushnames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldeditBrushNames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
